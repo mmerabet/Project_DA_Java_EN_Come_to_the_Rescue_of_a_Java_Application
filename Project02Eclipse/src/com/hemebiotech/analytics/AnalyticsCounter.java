65b8732ffd9acb6fbdfdb710f6a5b656fdff10d3
mmerabet <mhmd.merabet@gmail.com>
2020-03-03 18:36:20 +0700

    Utilisation de ReadSymptomDataFromFile

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,9 +1,9 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.*;
+
+import com.hemebiotech.analytics.read.ReadSymptomDataFromFile;
 
 public class AnalyticsCounter {
     //1ere étape: On déclare une HashMap clé/valeur <> symptoms/occurences
@@ -12,19 +12,17 @@ public class AnalyticsCounter {
 
     public static void main(String args[]) throws Exception {
         //2eme étape: On lit le fichier symptoms.txt
-        BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-        String line = reader.readLine();
+        ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile("symptoms.txt");
+        List<String> allSymptoms = reader.GetSymptoms();
 
         //3eme étape: On parcours le fichier en comptant les symptoms avec la map.
-        while (line!=null){
-            if (symptomsCounter.containsKey(line)) {
-                symptomsCounter.put(line,symptomsCounter.get(line)+1);
+        for (String symptom : allSymptoms){
+            if (symptomsCounter.containsKey(symptom)) {
+                symptomsCounter.put(symptom,symptomsCounter.get(symptom)+1);
             } else {
-                symptomsCounter.put(line,1);
+                symptomsCounter.put(symptom,1);
             }
-            line = reader.readLine();
         }
-        reader.close();
         
         //4eme étape: On range dans l'ordre alphabétique les symtoms.
         List<String> symptoms = new ArrayList<>(symptomsCounter.keySet());
