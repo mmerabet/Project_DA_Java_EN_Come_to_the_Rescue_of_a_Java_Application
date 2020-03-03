29bca2748c3a5d1a552b6367f9c92565f21d00dd
mmerabet <mhmd.merabet@gmail.com>
2020-03-03 17:54:49 +0700

    Correction du travail d'Alex en Hotfix.

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,41 +3,38 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.*;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+    //1ere étape: On déclare une HashMap clé/valeur <> symptoms/occurences
+    static Map<String, Integer>  symptomsCounter = new HashMap<>();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+    public static void main(String args[]) throws Exception {
+        //2eme étape: On lit le fichier symptoms.txt
+        BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+        String line = reader.readLine();
+
+        //3eme étape: On parcours le fichier en comptant les symptoms avec la map.
+        while (line!=null){
+            if (symptomsCounter.containsKey(line)) {
+                symptomsCounter.put(line,symptomsCounter.get(line)+1);
+            } else {
+                symptomsCounter.put(line,1);
+            }
+            line = reader.readLine();
+        }
+        reader.close();
+        
+        //4eme étape: On range dans l'ordre alphabétique les symtoms.
+        List<String> symptoms = new ArrayList<>(symptomsCounter.keySet());
+        Collections.sort(symptoms);
+
+        //5eme étape: On écrit le fichier result.out
+        FileWriter writer = new FileWriter ("result.out");
+        for (String symptom: symptoms){
+            writer.write(symptom+"="+symptomsCounter.get(symptom)+"\n");
+        }
+        writer.close();
+    }
 }
